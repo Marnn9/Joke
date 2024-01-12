5a2a7a08ab6fc257f4ff4fc5701ea815d94ccddd
Marnn9 <maren.narvik@gmail.com>
2024-01-12 11:33:25 +0100

    updated the code including Package.jason ++ and changes to improve the app

--- app.mjs
+++ app.mjs
@@ -1,25 +1,24 @@
 import chalk from "chalk";
 
-let colorValueA, colorValueB, colorValueC; 
-const randomValue = Math.floor(Math.random() * 255);
-
-function randomColor (){
-    colorValueA = randomValue;
-    colorValueB = randomValue;
-    colorValueC = randomValue;
+function randomColor() {
+    return Math.floor(Math.random() * 256); // Generate random values in the range [0, 255]
 }
 
 const joke = "Why don't skeletons fight each other? Because they don't have the guts!";
 const words = joke.split(' '); 
-let coloredText = '';
 
-words.forEach(function colorWords(word, index){
-    randomColor();
+const coloredText = words.reduce((accumulator, word, index) => {
+    const colorValueA = randomColor();
+    const colorValueB = randomColor();
+    const colorValueC = randomColor();
+
     const color = chalk.rgb(colorValueA, colorValueB, colorValueC);
-    coloredText += color(word);
+    accumulator += color(word);
+    
     if(index !== words.length - 1) {
-        coloredText += ' '; // Add space between words except for the last word
+        accumulator += ' '; // Add space between words except for the last word
     }
-});
+    return accumulator;
+}, '');
 
 console.log(coloredText); // Output the colored text as a single line
